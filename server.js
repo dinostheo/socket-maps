4f82bdda48069c1fecd5692932d5cbc462a96a44
dinostheo <dinostheo@gmail.com>
2013-01-04 16:41:18 +0100

    Redis added
    Changes to read places from Redis and to place a marker by click.

--- server.js
+++ server.js
@@ -13,13 +13,24 @@ var app = express()
 //server.listen(port);
 server.listen(3000);
 
+var redis = require("redis"),
+        redisClient = redis.createClient();
+
 app.get('/', function (req, res) {
   	res.sendfile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection', function (socket) {
-	socket.on('message', function(data) {
-      	socket.broadcast.send(data);
+    socket.on('moveme', function(data) {
+        console.log(data);
+      	redisClient.srandmember("locations", function(error, reply){
+            io.sockets.emit("moveme", reply.toString());
+        });
+    });
+
+    socket.on("markerPlaced", function(location){
+        console.log(location);
+        socket.broadcast.emit('markerPlaced', location);
     });
 
     socket.on('disconnect', function() {
